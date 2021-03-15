00aa51639f2ce6f053b54b6de55e2852c5594449
RandleH <40454051+RandleH@users.noreply.github.com>
2021-03-15 13:03:17 +0800

    Add files via upload

--- Xcode_12/Xcode_12/main.c
+++ Xcode_12/Xcode_12/main.c
@@ -16,23 +16,36 @@ int main(int argc, const char * argv[]) {
     
     GUI_RefreashScreen();
     
-    __GUI_Window_t cfg;
-    cfg.area.xs     = 30;
-    cfg.area.ys     = 40;
-    cfg.area.height = 300;
-    cfg.area.width  = 500;
-    cfg.type        = kGUI_WindowType_macOS;
-    cfg.size        = 40;
-    cfg.appearance  = kGUI_Appearance_Dark;
-    cfg.title       = "Text";
+    __GUI_Window_t cfg1;
+    cfg1.area.xs     = 30;
+    cfg1.area.ys     = 40;
+    cfg1.area.height = 300;
+    cfg1.area.width  = 500;
+    cfg1.type        = kGUI_WindowType_macOS;
+    cfg1.size        = 40;
+    cfg1.appearance  = kGUI_Appearance_Dark;
+    cfg1.title       = "Text";
+    
+    __GUI_Window_t cfg2;
+    cfg2.area.xs     = 200;
+    cfg2.area.ys     = 140;
+    cfg2.area.height = 130;
+    cfg2.area.width  = 300;
+    cfg2.type        = kGUI_WindowType_macOS;
+    cfg2.size        = 40;
+    cfg2.appearance  = kGUI_Appearance_Light;
+    cfg2.title       = "Glucoo";
     
     
-    ID_t ID_Window1 = GUI_create_window(&cfg);
+    ID_t ID_Window1 = GUI_create_window(&cfg1);
+    ID_t ID_Window2 = GUI_create_window(&cfg2);
     
     GUI_insert_window( ID_Window1 );
+    GUI_insert_window( ID_Window2 );
     GUI_RefreashScreen();
     
     GUI_delete_window( ID_Window1 );
+    
     
 
     
