1dab725824f4cbbc70ed0b70192e1d36d84c2b38
Randle Helmslay <randle_h@RandledeMacBook-Air.local>
2021-04-18 17:18:28 +0800

    Add Xcode_11.

---
+++ SIM_macOS/Xcode_11/Xcode_11/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Xcode_11
+//
+//  Created by Randle Helmslay on 2021/4/18.
+//  Copyright © 2021 Randle Helmslay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+
